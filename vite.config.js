5bb6ac071d78bdeba4ad1cb3ea73b6693d2cc289
Minh Nhat Le <151120107+mikeblocky@users.noreply.github.com>
2025-08-15 01:23:35 +0700

    added package

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,4 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": { target: "http://localhost:4000", changeOrigin: true }
-    }
-  }
-});
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+export default defineConfig({ plugins: [react()] });
